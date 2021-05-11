3bccde68649837a9ab2e2fb1db082597f71b595f
Mats Lian Krane <mats_krane@hotmail.com>
2021-05-11 22:25:00 +0200

    xd

--- Javascript/posts.js
+++ Javascript/posts.js
@@ -1,18 +1,30 @@
-const api = "https://www.matskrane.no/wp-json/wp/v2/posts/?_embed=wp:featuredmedia";
-const output = document.querySelector('#output');
+const url = "https://www.matskrane.no/wp-json/wp/v2/posts";
+const postHTML = document.querySelector(".posts")
 
-fetch(api)
-.then (response => response.json())
-.then (data => listPosts(data))
-.error (error => console.error("This happened: "+error));
-
-function listPosts (posts) {
-  for (let post of posts) {
-    if (post.featured_media > 0) {
-      let videoLink = post._embedded["_emded=wp:featuredmedia"][0];
-      output.innerHTML += `<div>${videoLink}"</div>`;     
-    } else {
-      output.innerHTML += `<div>[Post id=${post.id} does not have a featured image]</div>`;   
+async function getPosts() {
+    try{
+        const response = await fetch(url);
+        const getPosts = await response.json();
+        listPosts(getPosts);
+        console.log(getPosts);
     }
-  }
+   
+    catch(error){
+        console.log(error);
+    }
+    
+}
+
+getPosts();
+
+function listPosts(posts){
+    posts.forEach(function(post){
+        postHTML.innerHTML += `
+        <a href="post.html?id=${post.id}">
+        ${post.title.rendered}</a>
+        ${post.content.rendered}       
+        </li>
+        `;
+    })
+
 }
