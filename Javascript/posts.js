d6560d18c2073e283ec6d9213d2e6662df4cbfbe
Matskrane <69842790+Matskrane@users.noreply.github.com>
2021-05-26 03:13:58 +0200

    xd

--- Javascript/posts.js
+++ Javascript/posts.js
@@ -28,7 +28,7 @@ function listPosts(posts){
         <h3>${post.title.rendered}</h3>
         </a>
         ${post.content.rendered}
-        <a href="specificpost.html">Click me</a>
+        <a class="button" href="specificpost.html?id=${post.id}">Go to post</a>
         </div>      
         </div>
 
@@ -36,3 +36,44 @@ function listPosts(posts){
     })
 
 }
+
+/* Next page or load more  */ 
+
+const loadMoreBtn = document.querySelector('button');
+let pageCounter = 1;
+let loadMoreUrl = `https://matskrane.no/wp-json/wp/v2/posts/?_embed=wp:featuredmedia&page=${pageCounter}`;
+
+function displayMoreMedia(posts){
+    posts.forEach(function(post){
+        postHTML.innerHTML += `
+
+    <div class="video">
+    <div class="background-card">
+    <a href="specificpost.html?id=${post.id}">
+    <h3>${post.title.rendered}</h3>
+    </a>
+    ${post.content.rendered}
+    <a class="button" href="specificpost.html?id=${post.id}">Go to post</a>
+    </div>      
+    </div>
+
+    `;
+  })
+}
+
+const lastPage = () => {
+  loadMoreBtn.innerHTML = "Last page";
+  loadMoreBtn.disabled = true;
+};
+
+const loadPosts = () => {
+  pageCounter++;
+  loadMoreUrl = `https://matskrane.no/wp-json/wp/v2/posts/?_embed=wp:featuredmedia&page=${pageCounter}`;
+  console.log(loadMoreUrl);
+  fetch(loadMoreUrl)
+    .then((response) => response.json())
+    .then((data) => displayMoreMedia(data))
+    .catch((error) => lastPage());
+};
+
+loadMoreBtn.addEventListener("click", loadPosts);
