fa4ce84aa98cf9732ddc2963b68a2a53e55157f9
Matskrane <69842790+Matskrane@users.noreply.github.com>
2021-05-30 01:43:11 +0200

    xd

--- Javascript/posts.js
+++ Javascript/posts.js
@@ -35,41 +35,19 @@ function listPosts(posts){
 
 }
 
-console.warn = () => {};
-
-
 
 /* Next page or load more  */ 
 
 const ViewMoreBtn = document.querySelector('button');
-
 let page = 1;
-let viewMore = `https://matskrane.no/wp-json/wp/v2/posts/?_embed=wp:featuredmedia&page=${page}`;
-
-function viewMorePosts(posts){
-    posts.forEach(function(post){
-        postHTML.innerHTML += `
-    <div class="video">
-    <div class="background-card">
-    <a href="specificpost.html?id=${post.id}">
-    <h3>${post.title.rendered}</h3>
-    </a>
-    ${post.content.rendered}
-    <a class="button" href="specificpost.html?id=${post.id}">Go to post</a>
-    </div>      
-    </div>
-    `;
-  })
-}
-
 
 const loadPosts = () => {
   page++;
-  viewMore = `https://matskrane.no/wp-json/wp/v2/posts/?_embed=wp:featuredmedia&page=${page}`;
+let viewMore = `https://matskrane.no/wp-json/wp/v2/posts/?_embed=wp:featuredmedia&page=${page}`;
   console.log(viewMore);
   fetch(viewMore)
     .then((response) => response.json())
-    .then((data) => viewMorePosts(data))
+    .then((data) => listPosts(data))
     .catch((error) => loadPosts());
     console.log(error);
 };
