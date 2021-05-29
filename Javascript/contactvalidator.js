1c9978e58689631d9d6c0956fb7ce330249dfa67
Matskrane <69842790+Matskrane@users.noreply.github.com>
2021-05-29 14:21:24 +0200

    xd

--- Javascript/contactvalidator.js
+++ Javascript/contactvalidator.js
@@ -7,27 +7,33 @@ $(document).ready(function () {
 
     var name = $('#name').val()
     var email = $('#email').val()
+    var subject = $('#subject').val()
     var message = $('#message').val()
-    var statusElm = $('.status')
-    statusElm.empty()
+    var statusMessage = $('.status')
+    statusMessage.empty()
 
-    if(name.length > 2) {
-        statusElm.append("<div> Name is valid </div>" ) 
+    if(name.length >= 5) {
+        statusMessage.append("<div class='valid'> Name is valid </div>" ) 
     } else {
-        statusElm.append("<div> Name is not valid </div>")
-        document.getElementsByClassName("status").style.color = "#ff0000";
+        statusMessage.append("<div class='not-valid'> Name is not valid </div>")
     }
 
     if(email.length > 5 && email.includes('@') && email.includes('.')) {
-        statusElm.append("<div> Email is valid </div>")
+        statusMessage.append("<div class='valid'> Email is valid </div>")
     } else {
-        statusElm.append("<div> Email is not valid </div>")
+        statusMessage.append("<div class='not-valid'> Email is not valid </div>")
     }
 
-    if(message.length > 10) {
-        statusElm.append("<div> Message is valid </div>")
+    if(subject.length >= 15) {
+        statusMessage.append("<div class='valid'> Subject is valid </div>")
     } else {
-        statusElm.append("<div> Message is not valid </div>")
+        statusMessage.append("<div class='not-valid'> Subject is not valid </div>")
+    }
+
+    if(message.length >= 25) {
+        statusMessage.append("<div class='valid'> Message is valid </div>")
+    } else {
+        statusMessage.append("<div class='not-valid'> Message is not valid </div>")
     }
     
   })
